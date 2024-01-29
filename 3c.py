b67581ed2b9eafcda338d8a841e22135d462a748
SinPi310 <155887474+SinPi310@users.noreply.github.com>
2024-01-29 18:48:50 +0100

    Update 3c.py

--- 3c.py
+++ 3c.py
@@ -7,18 +7,25 @@ def cache_result(file_path):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            result_data = None
+
             if os.path.exists(file_path):
                 with open(file_path, 'rb') as file:
-                    result = pickle.load(file)
+                    result_data = pickle.load(file)
+
+            if result_data and result_data.get('args') == args:
+                result = result_data.get('result')
                 print(f'Wynik wczytany z pamięci podręcznej: {result}')
             else:
                 result = func(*args, **kwargs)
                 print(f'Wynik obliczony: {result}')
 
                 with open(file_path, 'wb') as file:
-                    pickle.dump(result, file)
-                    print(f'Wynik zapisany do pamięci podręcznej: {result}')
-            return result  # Zwracaj wynik funkcji
+                    new_data = {'args': args, 'result': result}
+                    pickle.dump(new_data, file)
+                    print(f'Nowe dane zapisane do pamięci podręcznej: {new_data}')
+
+            return result
 
         return wrapper
 
@@ -34,12 +41,16 @@ dataset = required_data()
 df = pd.DataFrame(dataset['train'])
 
 @cache_result(file_path='ten_oldest_clients_cache.pkl')
-def ten_oldest_clients():
-    top_10_oldest = df.nlargest(10, 'age')
+def ten_oldest_clients(x):
+    top_10_oldest = df.nlargest(x, 'age')
     selected_columns = ['limit_bal', 'age'] + [f'education:{i}' for i in range(7)]
     top_10_oldest = top_10_oldest[selected_columns]
     print(top_10_oldest)
-    return top_10_oldest  # Zwracaj wynik funkcji
+    return top_10_oldest
 
 if __name__ == '__main__':
-    ten_oldest_clients_result = ten_oldest_clients()
+    ##ten_oldest_clients_result = ten_oldest_clients(10)
+
+    df = pd.DataFrame(required_data()['train'])  # Wczytywanie zaktualizowane dane
+    ten_oldest_clients_result_new = ten_oldest_clients(3)
+    ##ten_oldest_clients_result_new = ten_oldest_clients(6)
